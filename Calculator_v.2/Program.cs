ea37fe7996506511d614c08c33f301a8d95f852c
Vladimir <morozvol>
2019-02-05 10:44:46 +0300

    add simplify

--- Calculator_v.2/Program.cs
+++ Calculator_v.2/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Runtime.InteropServices;
+using System.Globalization;
 
 namespace Calculator_v._2
 {
@@ -9,6 +8,57 @@ namespace Calculator_v._2
     {
         static void Main(string[] args)
         {
+            string task;
+            task = Console.ReadLine();
+
+
+
+            Console.ReadKey();
         }
+
+        public static string simplify(string task)
+        {
+
+            if (task.Split('(', ')').Length % 2 == 0)
+            {
+                Console.WriteLine("ошибка скобок");
+                return "Error";
+            }
+            else
+            {
+                foreach (var c in task)
+                {
+                    Console.WriteLine(1);
+                }
+                return task;
+            }
+        }
+    }
+
+    class Calculator
+    {
+        public static double Calculate(char option, double number1, double number2)
+        {
+            switch (option)
+            {
+                case '+':
+                    return number1 + number2;
+              
+                case '-':
+                    return number1 - number2;
+                   
+                case '*':
+                   return number1 * number2;
+                  
+                case '/':
+                    if (number2 == 0)
+                    {
+                        Console.WriteLine("делить на ноль нельзя");
+                    }
+                   break;
+            }
+            return number1 / number2;
+        }
+
     }
 }
